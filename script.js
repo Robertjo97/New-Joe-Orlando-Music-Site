ce5f0818c85554f90148092634a7faa909e29e64
Robertjo97 <robertjo97@gmail.com>
2023-12-31 13:08:08 -0800

    Add ticket prototype

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-let firstTitle = document.getElementById('firstTitle');
-let ticket = document.getElementById('ticket');
+let writeTicket = () => {
+  let ticket = document.getElementById("ticketText");
+  ticket.innerHTML =
+    "<h1>Next Show:</h1><h3>July 8, 2023 6:30pm-9:30pm</h3><h4>Brickyard Counter & Bar, El Dorado Hills, CA<h4><h4>Lando Collective</h4>";
+};
+
+writeTicket();
