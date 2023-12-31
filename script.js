70fd14aa70356a3c6a8cba6cfd7c888a834b81cb
Robertjo97 <robertjo97@gmail.com>
2023-12-31 15:20:58 -0800

    Mobile styling ticket

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let writeTicket = () => {
   let ticket = document.getElementById("ticketText");
   ticket.innerHTML =
-    "<h1>Next Show:</h1><h3>July 8, 2023 6:30pm-9:30pm</h3><h4>Brickyard Counter & Bar, El Dorado Hills, CA<h4><h4>Lando Collective</h4>";
+    "<h1>Next Show:</h1><h3>July 8, 2023 6:30pm-9:30pm</h3><h3>Brickyard Counter & Bar, El Dorado Hills, CA<h3><h3>Lando Collective</h3>";
 };
 
 writeTicket();
