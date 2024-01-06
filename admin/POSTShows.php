60b8cef63c874750afdd094a74d2a92ccb0d08ba
Robertjo97 <robertjo97@gmail.com>
2024-01-06 15:54:15 -0800

    Modify POSTShows.php

--- admin/POSTShows.php
+++ admin/POSTShows.php
@@ -11,7 +11,7 @@ class Show {
     }
 }
 
-$rawData = file_get_contents('./shows.json');
+$rawData = file_get_contents('../shows.json');
 $shows = json_decode($rawData, true);
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
@@ -24,7 +24,30 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $shows[] = array("date" => $show->date, "location" => $show->location, "band" => $show->band);
 
         $newShow = json_encode($shows, JSON_PRETTY_PRINT);
-        file_put_contents('./shows.json', $newShow);
+        file_put_contents('../shows.json', $newShow);
+
+        echo '<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>admin</title>
+            <link type="text/css" rel="stylesheet" href="./styles.css" />
+          </head>
+          <body>
+            <h3 id="showsTitle">Shows</h3>
+            <div id="showsList"></div>
+            <form id="addShowForm" method="post" action="./POSTShows.php">
+              <h4>Add Show</h4>
+              <input type="text" name="date" placeholder="Date">
+              <input type="text" name="location" placeholder="Location">
+              <input type="text" name="band" placeholder="Band">
+              <button id="addShowBtn">Submit</button>
+            </form>
+            <script src="./admin.js"></script>
+          </body>
+        </html>
+        ';
     }
 }
 ?>
