35a891bacf3518a79324028561bbade794c74716
Robertjo97 <robertjo97@gmail.com>
2024-01-06 12:29:50 -0800

    Modify admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,32 +1,48 @@
 let display = (shows) => {
-    let table = document.createElement('table');
-    let container = document.getElementById('showsList');
-    for(let i = 0; i < shows.length; i++){
-        let tr = document.createElement('tr');
-        let date = document.createElement('td');
-        let location = document.createElement('td');
-        let band = document.createElement('td');
-        date.innerHTML = shows[i].date;
-        location.innerHTML = shows[i].location;
-        band.innerHTML = shows[i].band;
-        tr.appendChild(date);
-        tr.appendChild(location);
-        tr.appendChild(band);
-        table.appendChild(tr);
+  let table = document.createElement("table");
+  let container = document.getElementById("showsList");
+  for (let i = -1; i < shows.length; i++) {
+    let tr = document.createElement("tr");
+    if (i == -1) {
+      let thDate = document.createElement("th");
+      let thLocation = document.createElement("th");
+      let thBand = document.createElement("th");
+
+      thDate.innerHTML = "Date";
+      thLocation.innerHTML = "Location";
+      thBand.innerHTML = "Band";
+
+      tr.appendChild(thDate);
+      tr.appendChild(thLocation);
+      tr.appendChild(thBand);
+      table.appendChild(tr);
+    } 
+    else {
+      let date = document.createElement("td");
+      let location = document.createElement("td");
+      let band = document.createElement("td");
+      date.innerHTML = shows[i].date;
+      location.innerHTML = shows[i].location;
+      band.innerHTML = shows[i].band;
+      tr.appendChild(date);
+      tr.appendChild(location);
+      tr.appendChild(band);
+      table.appendChild(tr);
     }
-    container.appendChild(table);
-}
+  }
+  container.appendChild(table);
+};
 
 let listShows = () => {
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = () => {
-        if(request.readyState == 4 && request.status == 200){
-            shows = JSON.parse(request.responseText);
-            display(shows);
-        }
+  let request = new XMLHttpRequest();
+  request.onreadystatechange = () => {
+    if (request.readyState == 4 && request.status == 200) {
+      shows = JSON.parse(request.responseText);
+      display(shows);
     }
-    request.open('GET', './GETShows.php');
-    request.send();
-}
+  };
+  request.open("GET", "./GETShows.php");
+  request.send();
+};
 
 listShows();
